6140327d1bca125b1f98ca6e76f53b9f49e2adf0
mergwyn <gary@theclarkhome.com>
2019-06-24 15:40:31 +0100

    Update sha256

--- Casks/unison248.rb
+++ Casks/unison248.rb
@@ -1,6 +1,6 @@
 cask 'unison248' do
   version '2.48.15'
-  sha256 'd578196d8b38f35c1e0410a1c86ff4e115a91f7eb211201db7a940a3a3e0f099'
+  sha256 '89894d14c9ff3c4d6195cb6a8065a2849e6ad55951799eedf8879e1a257d3e11'
 
   # github.com/bcpierce00/unison was verified as official when first introduced to the cask
   url "https://github.com/bcpierce00/unison/releases/download/#{version}/Unison-OS-X-#{version}.zip"
