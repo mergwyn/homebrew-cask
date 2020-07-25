0033b976de368a380cef449047e9311dbc869943
mergwyn <gary@theclarkhome.com>
2020-07-25 16:47:14 +0100

    Add additional versions

---
+++ Casks/unison252.rb
@@ -0,0 +1,19 @@
+cask 'unison252' do
+  version '2.51.2'
+  sha256 '0738a6978fa29bb2af409322069cc20df293b770877942ac4b8774f06e774aa5'
+
+  # github.com/bcpierce00/unison was verified as official when first introduced to the cask
+  url "https://github.com/bcpierce00/unison/releases/download/v#{version}/Unison-OS-X-#{version}.zip"
+  appcast 'https://github.com/bcpierce00/unison/releases.atom'
+  name 'Unison'
+  homepage 'https://www.cis.upenn.edu/~bcpierce/unison/'
+
+  conflicts_with formula: 'unison'
+
+  app 'Unison.app'
+  binary "#{appdir}/Unison.app/Contents/MacOS/cltool", target: 'unison'
+
+  postflight do
+    system_command '/usr/bin/defaults', args: ['write', 'edu.upenn.cis.Unison', 'CheckCltool', '-bool', 'false']
+  end
+end
