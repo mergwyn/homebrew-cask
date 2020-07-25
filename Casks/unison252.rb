1889863e7531fe5d72321a0ca27ce086de430e81
mergwyn <gary@theclarkhome.com>
2020-07-25 16:49:03 +0100

    Fix download URL

--- Casks/unison252.rb
+++ Casks/unison252.rb
@@ -3,7 +3,7 @@ cask 'unison252' do
   sha256 '0738a6978fa29bb2af409322069cc20df293b770877942ac4b8774f06e774aa5'
 
   # github.com/bcpierce00/unison was verified as official when first introduced to the cask
-  url "https://github.com/bcpierce00/unison/releases/download/v#{version}/Unison-OS-X-#{version}.zip"
+  url "https://github.com/bcpierce00/unison/releases/download/v#{version}/Unison-#{version}.OS.X.zip"
   appcast 'https://github.com/bcpierce00/unison/releases.atom'
   name 'Unison'
   homepage 'https://www.cis.upenn.edu/~bcpierce/unison/'
